501f30f95b36cbcc716e2871d3285b2cadd0340f
sakaguchi0713 <sakaguchi0713@gmail.com>
2018-10-29 21:59:20 +0900

    added connection database

--- api/api.go
+++ api/api.go
@@ -1,26 +1,63 @@
 package api
 
 import (
+	"github.com/jinzhu/gorm"
 	"github.com/riona/blog/model"
+	"github.com/riona/blog/shared/db"
+	"log"
 	"time"
 )
 
-type articleFinder interface {
-	FindArticleForIndex() (map[time.Time]string, error)
+type ArticleFinder interface {
+	FindArticleForIndex() (map[time.Time][]string, error)
 	FindArticleByDate(postTime time.Time) ([]model.Article, error)
 	FindAllArticles() ([]model.Article, error)
 }
 
-type finder struct{}
-
-func (p *finder) FindArticleForIndex() (map[time.Time]string, error) {
-	return nil, nil
+type finder struct {
+	db *gorm.DB
 }
 
-func (p *finder) FindArticleByDate(postTime time.Time) ([]model.Article, error) {
-	return nil, nil
+func NewFinder() (*finder, error) {
+	c, err := db.NewConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := c.Connect()
+	if err != nil {
+		return nil, err
+	}
+
+	f := &finder{db}
+	return f, nil
 }
 
-func (p *finder) FindAllArticles() ([]model.Article, error) {
-	return nil, nil
+func (f *finder) FindArticleForIndex() map[time.Time][]string {
+	var articles []model.Article
+	f.db.Find(&articles)
+
+	index := map[time.Time][]string{}
+	for _, art := range articles {
+		_, ok := index[art.Date]
+		if !ok {
+			index[art.Date] = []string{art.Title}
+		} else {
+			index[art.Date] = append(index[art.Date], art.Title)
+		}
+	}
+
+	return index
+}
+
+func (f *finder) FindArticleByDate(postTime time.Time) []model.Article {
+	var articles []model.Article
+	f.db.Find(&articles, "date=?", postTime)
+	return articles
+}
+
+func (f *finder) FindAllArticles() []model.Article {
+	var articles []model.Article
+	f.db.Find(&articles)
+	return articles
 }
--- shared/db/db_connection.go
+++ shared/db/db_connection.go
@@ -1,1 +1,31 @@
 package db
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/riona/blog/config"
+	"log"
+)
+
+type Connection struct {
+	config *config.Config
+}
+
+func NewConnection() (*Connection, error) {
+	config, err := config.NewConfig()
+	if err != nil {
+		return nil, err
+	}
+	c := &Connection{config: config}
+	return c, nil
+}
+
+func (c *Connection) Connect() (*gorm.DB, error) {
+	config := c.config
+	data := "%s:%s@/%s?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open(config.Adapter, data, config.User, config.Password, config.Database)
+	if err != nil {
+		log.Fatalf("Error can't Connection db err: %v", err)
+		return nil, err
+	}
+	return db, nil
+}
