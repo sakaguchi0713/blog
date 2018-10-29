391b9bebd43c69bcfb2dc19b250740ccf90dcd68
sakaguchi0713 <sakagucchisifon0713@gmail.com>
2018-10-29 19:40:22 +0900

    added GetArticle interface

--- api/api.go
+++ api/api.go
@@ -2,16 +2,20 @@ package api
 
 import (
 	"github.com/riona/blog/model"
+	"time"
 )
 
 type articleGetter interface {
-	GetIndexs() ([]model.Article, error)
+	GetIndex() (map[time.Time]string, error)
+	GetArticle(postTime time.Time) ([]model.Article, error)
 }
-
-
 
 type publisher struct {}
 
-func (p *publisher) GetIndex() ([]model.Article, error) {
+func (p *publisher) GetIndex() (map[time.Time]string, error) {
+	return nil, nil
+}
 
+func (p *publisher) GetArticle(postTime time.Time) ([]model.Article, error) {
+	return nil, nil
 }
