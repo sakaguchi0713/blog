ec7957dd5c57e866ab83c000de0d7dc8b2e4f5bc
sakaguchi0713 <sakaguchi0713@gmail.com>
2018-11-05 23:03:03 +0900

    try markdown

--- router/router.go
+++ router/router.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 	"github.com/riona/blog/controller/article"
 	"strconv"
+	"context"
+	"github.com/google/go-github/github"
+	"os"
+	"html/template"
 )
 
 func main() {
@@ -19,9 +23,33 @@ func main() {
 			c.String(http.StatusBadRequest, "caused error new finder")
 		}
 
+		md := []byte("## hogehoge document")
+		client := github.NewClient(nil)
+		opt := &github.MarkdownOptions{Mode: "gfm", Context: "google/go-github"}
+		body, _, err := client.Markdown(context.Background(), string(md), opt)
+
+		t, err := template.New("../assets/templates/index.tmpl").Parse(body)
+		if err != nil {
+			c.String(http.StatusBadRequest, "caused error parse err: %v", err)
+		}
+
+		data := struct {
+			Escaped    string
+			NotEscaped template.HTML
+		}{
+			Escaped:    "<b>escaped<b>",
+			NotEscaped: "<b>not escaped</b>",
+		}
+
+		err = t.Execute(os.Stdout, data)
+		if err != nil {
+			c.String(http.StatusBadRequest, "caused error execute err: %v", err)
+		}
+
 		articles := finder.FindArticleForIndex()
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"articles": articles,
+			"output":   body,
 		})
 	})
 
@@ -41,10 +69,14 @@ func main() {
 
 		c.HTML(http.StatusOK, "show.tmpl", gin.H{
 			"title": article.Title,
-			"body": article.Body,
-			"time": article.Date,
+			"body":  article.Body,
+			"time":  article.Date,
 		})
 	})
 
 	router.Run(":8080")
+}
+
+type pageData struct {
+	Body string
 }
